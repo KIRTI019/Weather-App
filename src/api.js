dc63a156a9bfc5d744b41859444fbe880ff7c010
KIRTI019 <113838803+KIRTI019@users.noreply.github.com>
2023-03-22 21:55:40 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "95208689ccmsh9f4f656e0e37cdcp1f1b12jsn99e51d67fef9",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "19c17111ace4b55b6b415979e6bea55a";
