7504fb9be3e7f04f2763f2fa11348a987029edb2
KIRTI019 <113838803+KIRTI019@users.noreply.github.com>
2023-09-23 11:57:12 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,5 +7,5 @@ export const geoApiOptions = {
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
-  export const WEATHER_API_URL = "https://api.openweathermap.org/data";
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
   export const WEATHER_API_KEY = "19c17111ace4b55b6b415979e6bea55a";
